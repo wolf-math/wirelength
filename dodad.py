ceb245a86a61a31d1c8a1ac906b069e853b87259
aaronwolf <aaron.wolf@gmail.com>
2020-03-29 09:38:52 +0300

    dodad input measurements

---
+++ dodad.py
@@ -0,0 +1,16 @@
+class Dodad(x_offset, y_offset):
+    input = {
+        in_1 = [x_offset, y_offset, 0],
+        in_2 = [x_offset + 10, y_offset, 0],
+        in_3 = [x_offset + 20, y_offset, 0],
+        in_4 = [x_offset + 30, y_offset, 0],
+        in_5 = [x_offset + 40, y_offset, 0],
+        in_6 = [x_offset + 50, y_offset, 0],
+        in_7 = [x_offset + 60, y_offset, 0],
+        in_8 = [x_offset + 70, y_offset, 0],
+        in_9 = [x_offset + 80, y_offset, 0],
+        in_10 = [x_offset + 90, y_offset, 0],
+        in_11 = [x_offset + 100, y_offset, 0],
+        in_12 = [x_offset + 110, y_offset, 0],
+        }
+        
